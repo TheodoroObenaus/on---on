80725e1aa6d86b94599d5ab1f19d2defb48434cb
TheodoroObenaus <theodoro.obenaus@gmail.com>
2023-03-27 11:46:30 -0300

    gangna style

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import { ref } from 'vue'
+const texto = ref('')
+
+</script>
+
+<template>
+
+      <div class="container">
+        <input type="text" v-model="texto"/>
+        <p>Texto digitado: {{ texto }}</p>
+    </div>
+
+</template>
+
+<style scoped>
+</style>
